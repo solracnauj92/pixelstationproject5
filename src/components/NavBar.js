bc8d9f82649f4d48384eae1dc690e200c2b4a29a
Juan Carlos Diaz Lara <juancarlosdiazlara@gmail.com>
2024-06-28 19:13:50 +0000

    Setup responsive navbar using Bootstrap components, logo and icons. Include CSS styling and added Bootstrap stylesheet to connect

---
+++ src/components/NavBar.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import logo from "../assets/logo.png";
+import styles from '../styles/Navbar.module.css'
+
+
+
+const NavBar = () => {
+  return (
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
+      <Container>
+        <Navbar.Brand href="#">
+          <img src={logo} alt="logo" height="150" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto text-left">
+            <Nav.Link>
+            <i class="bi bi-house"></i> Home
+            </Nav.Link>
+            <Nav.Link>
+                <i class="bi bi-person-check"></i> Sign in
+            </Nav.Link>
+            <Nav.Link>
+                <i class="bi bi-person-plus"></i> Sign up
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavBar;
