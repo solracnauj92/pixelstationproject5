b5e8e3c6c5edfda457310cfd626e9dd02d43d0e3
Juan Carlos Diaz Lara <juancarlosdiazlara@gmail.com>
2024-07-14 21:45:08 +0000

    Add SignUpForm component and update styles

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+axios.defaults.baseURL = 'https://pixelstationproject5-api-1a9dadf46f0b.herokuapp.com/';
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
+axios.defaults.withCredentials = true ;
