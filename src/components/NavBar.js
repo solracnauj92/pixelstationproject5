d7d43dfc1458f94ae40e8982f6ef5ddd92df6127
Juan Carlos Diaz Lara <juancarlosdiazlara@gmail.com>
2024-07-18 15:41:21 +0000

    Refactor code to utilize custom context hook

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,41 +1,53 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/logo.png";
-import styles from '../styles/Navbar.module.css'
+import styles from "../styles/Navbar.module.css"
 import { NavLink } from "react-router-dom";
-
+import { useCurrentUser } from "../contexts/CurrentUserContext";
 
 const NavBar = () => {
+  const currentUser = useCurrentUser();
+
+  const loggedInIcons = <>{currentUser?.username}</>;
+  const loggedOutIcons = (
+    <>
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/signin"
+      >
+        <i className="bi bi-person-check"></i> Sign in
+      </NavLink>
+      <NavLink
+        to="/signup"
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+      >
+        <i className="bi bi-person-plus"></i> Sign up
+      </NavLink>
+    </>
+  );
+
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
         <NavLink to="/">
-        <Navbar.Brand>
-          <img src={logo} alt="logo" height="150" />
-        </Navbar.Brand></NavLink>
+          <Navbar.Brand>
+            <img src={logo} alt="logo" height="150" />
+          </Navbar.Brand>
+        </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
-            <NavLink 
-            exact
-            className={styles.NavLink } 
-            activeClassName={styles.Active} 
-            to="/"
+            <NavLink
+              exact
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/"
             >
-            <i class="bi bi-house"></i> Home
+              <i className="bi bi-house"></i> Home
             </NavLink>
-            <NavLink 
-            className={styles.NavLink }
-            activeClassName={styles.Active} 
-            to="/signin">
-                <i class="bi bi-person-check"></i> Sign in
-            </NavLink>
-            <NavLink 
-            className={styles.NavLink }
-            activeClassName={styles.Active} 
-            to="/signup">
-                <i class="bi bi-person-check"></i> Sign up
-            </NavLink>
+            {currentUser ? loggedInIcons : loggedOutIcons}
           </Nav>
         </Navbar.Collapse>
       </Container>
