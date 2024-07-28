e0feab02f38af88fdc67a4e927f59b089dacaad1
Juan Carlos Diaz Lara <juancarlosdiazlara@gmail.com>
2024-07-28 20:16:10 +0000

    Fix: Corrected default export in App.js and installed @babel/plugin-proposal-private-property-in-object to resolve build error

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import React, { useState } from "react";
 import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
 import Container from "react-bootstrap/Container";
 import { Route, Switch } from "react-router-dom";
+import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
 
 function App() {
-  const [currentUser, setCurrentUser] = useState(null);
-
   return (
     <div className={styles.App}>
       <NavBar />
--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -14,8 +14,12 @@ import { Link, useHistory } from "react-router-dom";
 import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
+import { useSetCurrentUser } from "../../contexts/CurrentUserContext";
+
 
 function SignInForm() {
+  const setCurrentUser = useSetCurrentUser();
+
   const [signInData, setSignInData] = useState({
     username: "",
     password: "",
