bb6d616bb0ba1f75da5c870c095cbaf633c54534
Juan Carlos Diaz Lara <juancarlosdiazlara@gmail.com>
2024-10-10 18:58:31 +0000

    Add edit functionality to Comment component with dropdown menu support

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -75,8 +75,17 @@ function PostPage() {
 
             comments.results.map((comment) => (
 
-              <Comment key={comment.id} {...comment} />
+              <Comment
 
+                key={comment.id}
+
+                {...comment}
+
+                setPost={setPost}
+
+                setComments={setComments}
+
+              />
             ))
 
           ) : currentUser ? (
