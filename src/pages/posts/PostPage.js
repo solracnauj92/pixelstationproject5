9260c339bde17dc7d90de6574777ef527dc2335e
Juan Carlos Diaz Lara <juancarlosdiazlara@gmail.com>
2024-10-11 18:33:55 +0000

    Implement unfollow functionality and sync states. Added handleUnfollow, unfollowHelper, and updated PostPage.

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -19,6 +19,8 @@ import InfiniteScroll from "react-infinite-scroll-component";
 import Asset from "../../components/Asset";
 import { fetchMoreData } from "../../utils/utils";
 
+import PopularProfiles from "../profiles/PopularProfiles";
+
 function PostPage() {
   const { id } = useParams();
   const [post, setPost] = useState({ results: [] });
@@ -50,7 +52,7 @@ function PostPage() {
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
-        <p>Popular profiles for mobile</p>
+      <PopularProfiles mobile />
         <Post {...post.results[0]} setPosts={setPost} postPage />
         <Container className={appStyles.Content}>
 
@@ -120,7 +122,7 @@ function PostPage() {
 
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
-        Popular profiles for desktop
+      <PopularProfiles />
       </Col>
     </Row>
   );
