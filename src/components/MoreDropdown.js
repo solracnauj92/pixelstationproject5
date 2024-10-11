bf6fcb1a0bfdae4245d7c1ecf36612052a676a33
Juan Carlos Diaz Lara <juancarlosdiazlara@gmail.com>
2024-10-11 19:05:59 +0000

    Attempt to fix by adding missing codes to #MoreDropdown.js and ProfilePage.js

--- src/components/MoreDropdown.js
+++ src/components/MoreDropdown.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Dropdown from "react-bootstrap/Dropdown";
 import styles from "../styles/MoreDropdown.module.css";
+import { useHistory } from "react-router";
 
 
 // The forwardRef is important!!
@@ -41,5 +42,64 @@ export const MoreDropdown = ({ handleEdit, handleDelete }) => {
       </Dropdown.Menu>
     </Dropdown>
   );
+};
+export const ProfileEditDropdown = ({ id }) => {
+
+  const history = useHistory();
+
+  return (
+
+    <Dropdown className={`ml-auto px-3 ${styles.Absolute}`} drop="left">
+
+      <Dropdown.Toggle as={ThreeDots} />
+
+      <Dropdown.Menu>
+
+        <Dropdown.Item
+
+          onClick={() => history.push(`/profiles/${id}/edit`)}
+
+          aria-label="edit-profile"
+
+        >
+
+          <i className="fas fa-edit" /> edit profile
+
+        </Dropdown.Item>
+
+        <Dropdown.Item
+
+          onClick={() => history.push(`/profiles/${id}/edit/username`)}
+
+          aria-label="edit-username"
+
+        >
+
+          <i className="far fa-id-card" />
+
+          change username
+
+        </Dropdown.Item>
+
+        <Dropdown.Item
+
+          onClick={() => history.push(`/profiles/${id}/edit/password`)}
+
+          aria-label="edit-password"
+
+        >
+
+          <i className="fas fa-key" />
+
+          change password
+
+        </Dropdown.Item>
+
+      </Dropdown.Menu>
+
+    </Dropdown>
+
+  );
+
 };
 
