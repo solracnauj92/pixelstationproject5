ab831b30cc6760b626ae080d839ef5675e533c71
Juan Carlos Diaz Lara <juancarlosdiazlara@gmail.com>
2024-10-17 22:42:15 +0000

    Implement Token in utils.js

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,4 @@
+import jwtDecode from "jwt-decode";
 import { axiosReq } from "../api/axiosDefaults";
 
 export const fetchMoreData = async (resource, setResource) => {
@@ -81,6 +82,19 @@ export const unfollowHelper = (profile, clickedProfile) => {
 
       profile;
 
+};
+
+export const setTokenTimestamp = (data) => {
+  const refreshTokenTimestamp = jwtDecode(data?.refresh_token).exp;
+  localStorage.setItem("refreshTokenTimestamp", refreshTokenTimestamp);
+};
+
+export const shouldRefreshToken = () => {
+  return !!localStorage.getItem("refreshTokenTimestamp");
+};
+
+export const removeTokenTimestamp = () => {
+  localStorage.removeItem("refreshTokenTimestamp");
 };
 
 // Function to get the list of games from the API
