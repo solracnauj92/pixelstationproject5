cfeb1d40cb3fc6f39351cdc7628e4d97c56d1830
Juan Carlos Diaz Lara <juancarlosdiazlara@gmail.com>
2024-10-18 13:28:50 +0000

    Update routes in Forum  to correctly handle forum, thread, and post fetching

--- src/pages/forum/Forum.js
+++ src/pages/forum/Forum.js
@@ -2,28 +2,28 @@
 import React, { useState } from 'react';
 import ForumList from './ForumList';
 import ThreadList from './ThreadList';
-import PostList from './PostForum.js';
+import PostForum from './PostForum';  // Renamed to PostForum
 import ThreadCreateForm from './ThreadCreateForm';
 import styles from '../../styles/Forum.module.css';
 
 const Forum = () => {
   const [selectedForum, setSelectedForum] = useState(null);
   const [selectedThread, setSelectedThread] = useState(null);
-  const [showThreadCreateForm, setShowThreadCreateForm] = useState(false); // State to manage visibility of thread creation form
+  const [showThreadCreateForm, setShowThreadCreateForm] = useState(false);
 
   const handleForumSelect = (forumId) => {
     setSelectedForum(forumId);
     setSelectedThread(null);
-    setShowThreadCreateForm(true); // Show thread create form when a forum is selected
+    setShowThreadCreateForm(true);
   };
 
   const handleThreadSelect = (threadId) => {
     setSelectedThread(threadId);
-    setShowThreadCreateForm(false); // Hide thread create form when a thread is selected
+    setShowThreadCreateForm(false); 
   };
 
   const handleCloseThreadCreateForm = () => {
-    setShowThreadCreateForm(false); // Function to close the thread creation form
+    setShowThreadCreateForm(false); 
   };
 
   return (
@@ -40,7 +40,7 @@ const Forum = () => {
           )}
         </>
       )}
-      {selectedThread && <PostList threadId={selectedThread} />}
+      {selectedThread && <PostForum threadId={selectedThread} />} {/* Use PostForum here */}
     </div>
   );
 };
